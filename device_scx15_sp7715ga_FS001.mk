241199afaf9356ddaf1cc78f58a8821ec07ac05b
Qiangong2 <qiangong2@126.com>
2016-04-27 07:21:31 -0700

    Update device_scx15_sp7715ga_FS001.mk

--- device_scx15_sp7715ga_FS001.mk
+++ device_scx15_sp7715ga_FS001.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+LOCAL_PATH := device/BLU/D190U
 
 # get minimum languages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
