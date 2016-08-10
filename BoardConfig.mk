c0453ef2a99fe2e3a5a95aa87755a1b2295e7da7
Qiangong2 <qiangong2@126.com>
2016-08-10 15:35:22 -0700

    update BoardConfig.mk

---
+++ BoardConfig.mk
@@ -0,0 +1,24 @@
+#
+#Copyright (C) 2016 The Android Open-Source Project
+#
+#Licensed Under the Apache License, Version 2.0 (the "License");
+#You may only use this file in compliance with the License
+#
+#
+#This BoardConfig.mk was originally made by Qiangong2 for the D190U
+#
+#Do Not Edit any text above and including this line
+#Feel free to edit anything below this line :)
+#
+#
+# Thanks BLU for NOT providing the source code... -_-
+
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_BOARD_PLATFORM := scx15
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := false
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_BOOTLOADER_BOARD_NAME := scx15_sp7715ga
