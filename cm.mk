a3439407562480d02c3ce6962f75d13be857a618
Qiangong2 <qiangong2@126.com>
2016-08-10 15:19:50 -0700

    Update cm.mk

---
+++ cm.mk
@@ -0,0 +1,25 @@
+
+#cm.mk originally made by Qiangong2
+#	NOT by BLU
+
+#release name
+PRODUCT_RELEASE_NAME := scx15_sp7715ga_FS001
+
+#inherit common CM stuff
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+#inherit device configuration
+$(call inherit-product, device/BLU/D190U/full_d190u.mk)
+
+#Override build properties
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=scx15_sp7715ga_FS001plus_NativeUIhvga \
+	TARGET_DEVICE=scx15_sp7715ga_FS001 \
+	BUILD_FINGERPRINT="BLU/scx15_sp7715ga_FS001plus_NativeUIhvga/scx15_sp7715ga_FS001:4.4.2/KOT49H/BLU_D190U_V08_GENERIC_20150918_1512:user/test-keys" PRIVATE_BUILD_DESC="user_4.4.2_KOT49H-BLU_D190U_V08_GENERIC_20150918_1512+test-keys"
+
+#Device Identifier. Needs to come after every other inclusion
+PRODUCT_MODEL := DASH JR 3G
+PRODUCT_BRAND := BLU
+PRODUCT_NAME := cm_d190u
+PRODUCT_DEVICE := D190U
+PRODUCT_MANUFACTURER := BLU
